889d3f587381265d64a218865c32182f2eb6a75f
John Paul Woods <woodsjp@student.swosu.edu>
2016-11-05 02:39:12 -0500

    add problem information to 7.29

--- Homework_7/src/src/Problem_7_29.java
+++ Homework_7/src/src/Problem_7_29.java
@@ -1,9 +1,20 @@
+
+/* Problem 7.29
+ * Introduction to Java Programming, 10th Ed. 
+ * by Y Daniel Liang
+ * 
+ * (Game: pick four cards) Write a program that picks four 
+ * cards from a deck of 52 cards and computes their sum. 
+ * An Ace, King, Queen, and Jack represent 1, 13, 12, and 11, 
+ * respectively. Your program should display the number of 
+ * picks that yields the sum of 24.
+ */
+
 package src;
 
 public class Problem_7_29 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
