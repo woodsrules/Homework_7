43be478ba71cda339db9f30b42ab66a4576c8c46
John Paul Woods <woodsjp@student.swosu.edu>
2016-11-06 17:23:20 -0600

    add complete problem 8.11

--- Homework_7/src/src/Problem_8_11.java
+++ Homework_7/src/src/Problem_8_11.java
@@ -28,7 +28,43 @@ package src;
 public class Problem_8_11 {
 
 	public static void main(String[] args) {
+		int[][] m = new int[3][3];
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number between 0 and 511: ");
+		int n = input.nextInt();
+		String binary = decimalToBinaryModified(n);
+
+		// put 1's and 0's using binary string
+		int bIndex = 0; // binary string index
+		for (int i = 0; i < m.length; i++) {
+
+			for (int k = 0; k < m[i].length; k++) {
+				int coinSide = (binary.charAt(bIndex++) == '0') ? 0 : 1;
+				m[i][k] = coinSide;
+			}
+		}
+		for (int i = 0; i < m.length; i++) {
+
+			for (int k = 0; k < m[i].length; k++) {
+				char ch = (m[i][k] == 0) ? 'H' : 'T';
+				System.out.print(ch + " ");
+			}
+			System.out.println("");
+		}
 
 	}
 
+	public static String decimalToBinaryModified(int n) {
+
+		StringBuilder s = new StringBuilder();
+		while (n != 0) {
+			s.append(n & 1);
+			n = n >> 1;
+		}
+		while (s.length() < 9) {
+			s.insert(0, "0");
+		}
+		return s.toString();
+	}
 }
